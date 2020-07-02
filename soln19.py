c4aa144c2ec28015205fd188457682daa1282a2b
nduwal <duwalniru0@gmail.com>
2020-07-02 22:02:49 +0545

    removed unnecessary imports

--- soln19.py
+++ soln19.py
@@ -1,6 +1,5 @@
 # Write a Python class to find validity of a string of parentheses, '(', ')', '{', '}', '[' and ']. 
 # These brackets must be close in the correct order, for example "()" and "()[]{}" are valid but "[)", "({[)]" and "{{{" are invalid 
-import time
 
 
 class ParenthesesValidity:
