18024a9e9134ad8a5f3d735dd439f07a7de8fa0a
Samridh Tuladhar <tsamridh86@users.noreply.github.com>
2020-07-08 20:13:31 +0530

    Update soln2.py
    incorrect formula

--- soln2.py
+++ soln2.py
@@ -1,15 +1,9 @@
 # Write an if statement to determine whether a variable holding a year 
 # is a leap year. 
 
-given_year = int(input("Enter a year to check if it is leap or not:: "))
-if(given_year % 4 == 0):
-    if(given_year % 100 == 0):
-        if( given_year % 400 == 0 ):
-            print('{} is a leap year'.format(given_year))
-        else:
-            print('{} is not a leap year'.format(given_year))
-    else:
-         print('{} is a leap year'.format(given_year))
+year = int(input("Enter a year to check if it is leap or not:: "))
+if (( year % 400 == 0 ) or ( year % 4 == 0 and  year % 100 != 0 )):
+    print("leap year")
 else:
-    print('{} is not a leap year'.format(given_year))
+    print("not leap year")
 
